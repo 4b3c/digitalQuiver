4135ee207d5713e4ab199dbaa24be377d89194b9
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-14 19:51:51 -0700

    testing moving, and reorganized tank game files

--- birthdays/script.js
+++ birthdays/script.js
@@ -1,12 +1,18 @@
+var y = 400;
 
 function setup() {
-  createCanvas(windowWidth, 400);
+    createCanvas(windowWidth, 400);
 }
 
 function draw() {
     background(89, 213, 247);
     fill(255, 10, 10);
-    textAlign(CENTER);
+    textAlign(CENTER, TOP);
     textSize(40);
-    text("Happy Birthday\nCampbell!!", windowWidth * 0.5, 40);
+    text("Happy Birthday\nCampbell!!", windowWidth * 0.5, y);
+    if (y < -100) {
+        y = 400;
+    } else {
+        y--;
+    }
 }
