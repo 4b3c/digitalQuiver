245f6ae47b80888e915c7fda8078c21045e7727a
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-14 19:41:43 -0700

    more phone tests

--- birthdays/script.js
+++ birthdays/script.js
@@ -7,8 +7,6 @@ function draw() {
     background(89, 213, 247);
     fill(255, 10, 10);
     textAlign(CENTER);
-    textSize(50);
-    text("Happy Birthday Campbell!!", windowWidth * 0.5, 40);
-    ellipse(windowWidth * 0.1, 200, 80, 120);
-    ellipse(windowWidth * 0.3, 200, 80, 120);
+    textSize(40);
+    text("Happy Birthday\nCampbell!!", windowWidth * 0.5, 40);
 }
