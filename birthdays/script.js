92d1fe4eb360b84d28d66c11df1fce9771583c84
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-14 22:07:34 -0700

    oops

--- birthdays/script.js
+++ birthdays/script.js
@@ -1,9 +1,24 @@
+var y = 20;
 let img;
 
 function preload() {
-  img = loadImage("hamster.jpg");
+    img = loadImage('assets/soccerball.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, 400);
 }
 
 function draw() {
-  image(img, 0, 0, 300, 300);
+    background(89, 213, 247);
+    fill(255, 10, 10);
+    textAlign(CENTER, TOP);
+    textSize(40);
+    text("Happy Birthday\nCampbell!!", windowWidth * 0.5, y);
+    image(img, 0, 0, 200, 200);
+    if (y < -100) {
+        y = 400;
+    } else {
+        y--;
+    }
 }
