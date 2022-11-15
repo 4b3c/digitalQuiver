a754abb1a087808c75e8d5d7086cc2ba7b05292b
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-14 19:21:18 -0700

    testing phone compatability with text sizes

--- birthdays/script.js
+++ birthdays/script.js
@@ -1,12 +1,14 @@
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, 400);
 }
 
 function draw() {
     background(89, 213, 247);
     fill(255, 10, 10);
-    ellipse(100, 100, 80, 120);
-    ellipse(300, 100, 80, 120);
-    ellipse(200, 60, 30, 30);
+    textAlign(CENTER);
+    textSize(50);
+    text("Happy Birthday Campbell!!", windowWidth * 0.5, 40);
+    ellipse(windowWidth * 0.1, 200, 80, 120);
+    ellipse(windowWidth * 0.3, 200, 80, 120);
 }
