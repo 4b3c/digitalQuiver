58b185ccd0d36db3b9f7f5738cc9a031f4bf31ab
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-16 22:53:47 -0700

    you can max double jump now

--- birthdays/campbell.js
+++ birthdays/campbell.js
@@ -2,6 +2,8 @@ let img;
 var x = 20;
 var y = 0;
 var yvel = 5;
+var clicked = false;
+var clickcount = 0;
 
 function preload() {
     img = loadImage('soccerball.png');
@@ -18,19 +20,31 @@ function draw() {
     textSize(40);
     // text("Happy Birthday\nCampbell!!", windowWidth * 0.5, 20);
     image(img, x, y, 100, 100);
-    if (mouseIsPressed) {
-      yvel = 13;
+    if (mouseIsPressed && clickcount < 2) {
+      yvel = 21;
+      clicked = true;
+    } else if (clicked) {
+      clickcount = clickcount + 1;
+      clicked = false;
     }
+  text(clickcount, 300, 20);
+  
     y = y - yvel;
   
-    if (y > windowHeight - 100) {
-      y = windowHeight - 100;
+    if (y > windowHeight - 100 || y < 0) {
+      if (y > windowHeight - 100) {
+        y = windowHeight - 100;
+        clickcount = 0;
+      } else {
+        y = 0;
+      }
       if (Math.abs(yvel) > 2) {
-        yvel = -yvel * 0.5;
+        yvel = -yvel * 0.4;
       } else {
         yvel = 0;
       }
     } else {
-      yvel = yvel - 0.6;
+      yvel = yvel - 1.2;
     }
+
 }
