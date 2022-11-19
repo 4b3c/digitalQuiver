cfc80cea7d85283841d5e1d473de5f3a97f80286
Abram <58718191+4b3c@users.noreply.github.com>
2022-11-19 14:31:15 -0700

    made the ball fit correctly proportional in the screen

--- birthdays/campbell.js
+++ birthdays/campbell.js
@@ -5,6 +5,7 @@ var y = 100
 var ballwh = [60, 60];
 var first_click = false;
 var clicked = false;
+var score = 0;
 
 var obs1 = [0, 0, 0];
 var obs2 = [0, 0, 0];
@@ -15,19 +16,22 @@ var ins_text = ["instructions:", "tap the sides of the screen", "to dodge the ob
 //create a canvas the size of the window
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    x = round(((windowWidth / 2) - (ballwh[0] / 2)) / 60) * 60;
+    ballwh[0] = windowWidth / 11;
+    ballwh[1] = ballwh[0];
+    x = round(windowWidth / ballwh[0]) * ballwh[0];
+    x = x - (ballwh[0] * 6);
     y = windowHeight - (ballwh[1] * 2);
     obs1[1] = -windowHeight * 1/3;
     obs2[1] = -windowHeight * 2/3;
     obs3[1] = -windowHeight;
   
-    obs1[2] = random(100, 200);
-    obs2[2] = random(100, 200);
-    obs3[2] = random(100, 200);
+    obs1[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
+    obs2[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
+    obs3[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
   
-    obs1[0] = random(0, windowWidth - obs1[2]);
-    obs2[0] = random(0, windowWidth - obs2[2]);
-    obs3[0] = random(0, windowWidth - obs3[2]);
+    obs1[0] = round(random(0, windowWidth - obs1[2]) / ballwh[0]) * ballwh[0];
+    obs2[0] = round(random(0, windowWidth - obs2[2]) / ballwh[0]) * ballwh[0];
+    obs3[0] = round(random(0, windowWidth - obs3[2]) / ballwh[0]) * ballwh[0];
 }
 
 //load the image of the soccerball
@@ -41,10 +45,10 @@ function manage_mouse() {
     if (mouseIsPressed && !clicked) {
         clicked = true;
         first_click = true;
-        if (mouseX > windowWidth / 2 && x + ballwh[0] < windowWidth - ballwh[0]) {
+        if (mouseX > windowWidth / 2 && x + ballwh[0] <= windowWidth - ballwh[0] + 1) {
             //move ball to the right
             x = x + ballwh[0];
-        } else if (mouseX < windowWidth / 2 && x - ballwh[0] >= 0) {
+        } else if (mouseX < windowWidth / 2 && x - ballwh[0] >= -1) {
             //move ball to the left
             x = x - ballwh[0];
         }
@@ -58,10 +62,11 @@ function show_instructions() {
     if (!first_click) {
         fill(0);
         textAlign(CENTER, CENTER);
-        textSize(30);
+        textSize(windowWidth / 15);
         text(ins_text[0], windowWidth / 2, 100);
-        text(ins_text[1], windowWidth / 2, 150);
-        text(ins_text[2], windowWidth / 2, 200);
+        textSize(windowWidth / 18);
+        text(ins_text[1], windowWidth / 2, 160);
+        text(ins_text[2], windowWidth / 2, 220);
     } else {
         obs1[1] = obs1[1] + 5;
         obs2[1] = obs2[1] + 5;
@@ -83,32 +88,46 @@ function draw() {
     rect(obs2[0], obs2[1], obs2[2], 50);
     rect(obs3[0], obs3[1], obs3[2], 50);
   
+    fill(0);
+    textSize(windowHeight / 24);
+    textAlign(LEFT, TOP);
+    text("Points: " + score, 30, 50);
+  
     if (obs1[1] > windowHeight) {
-        obs1[2] = round(random(60, 280) / 60) * 60;
-        obs1[0] = round(random(0, windowWidth - obs1[2]) / 60) * 60;
+        obs1[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
+        obs1[0] = round(random(0, windowWidth - obs1[2]) / ballwh[0]) * ballwh[0];
         obs1[1] = -100;
+        score++;
     } else if (obs2[1] > windowHeight) {
-        obs2[2] = round(random(60, 280) / 60) * 60;
-        obs2[0] = round(random(0, windowWidth - obs2[2]) / 60) * 60;
+        obs2[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
+        obs2[0] = round(random(0, windowWidth - obs2[2]) / ballwh[0]) * ballwh[0];
         obs2[1] = -100;
+        score++;
     } else if (obs3[1] > windowHeight) {
-        obs3[2] = round(random(60, 280) / 60) * 60;
-        obs3[0] = round(random(0, windowWidth - obs3[2]) / 60) * 60;
+        obs3[2] = round(random(ballwh[0], ballwh[0] * 4) / ballwh[0]) * ballwh[0];
+        obs3[0] = round(random(0, windowWidth - obs3[2]) / ballwh[0]) * ballwh[0];
         obs3[1] = -100;
+        score++;
     }
   
 
-    if (obs1[1] + 60 > y && obs1[1] < y + ballwh[1]) {
-        if (obs1[0] + obs1[2] > x && obs1[0] < x + ballwh[0]) {
-            text("YOU DIED", 80, 30);
+    if (obs1[1] + ballwh[1] > y && obs1[1] < y + ballwh[1]) {
+        if (obs1[0] + obs1[2] - 1 > x && obs1[0] < x + ballwh[0] - 1) {
+            textSize(windowWidth / 10);
+            textAlign(CENTER, CENTER);
+            text("YOU DIED", windowWidth / 2, windowHeight / 2);
         }
-    } else if (obs2[1] + 60 > y && obs2[1] < y + ballwh[1]) {
-        if (obs2[0] + obs2[2] > x && obs2[0] < x + ballwh[0]) {
-            text("YOU DIED", 80, 30);
+    } else if (obs2[1] + ballwh[1] > y && obs2[1] < y + ballwh[1]) {
+        if (obs2[0] + obs2[2] - 1 > x && obs2[0] < x + ballwh[0] - 1) {
+            textSize(windowWidth / 10);
+            textAlign(CENTER, CENTER);
+            text("YOU DIED", windowWidth / 2, windowHeight / 2);
         }
-    } else if (obs3[1] + 60 > y && obs3[1] < y + ballwh[1]) {
-        if (obs3[0] + obs3[2] > x && obs3[0] < x + ballwh[0]) {
-            text("YOU DIED", 80, 30);
+    } else if (obs3[1] + ballwh[1] > y && obs3[1] < y + ballwh[1]) {
+        if (obs3[0] + obs3[2] - 1 > x && obs3[0] < x + ballwh[0] - 1) {
+            textSize(windowWidth / 10);
+            textAlign(CENTER, CENTER);
+            text("YOU DIED", windowWidth / 2, windowHeight / 2);
         }
     }
     
