a5fcf6849a8a0e254e5d47d2e0a4acce8c9da184
Abram <58718191+4b3c@users.noreply.github.com>
2023-04-26 19:09:15 -0700

    forgot to limit the ends

--- menu.js
+++ menu.js
@@ -1,8 +1,9 @@
 let rotation = 0;
 
 window.addEventListener("wheel", () => {
-  rotation += event.deltaY / 1100;
-  console.log(rotation);
+  rotation += event.deltaY / 1150;
+  if (rotation > 1) {rotation = 1}
+  if (rotation < 0) {rotation = 0}
   document.body.style.setProperty("--scroll", rotation);
 
 }, false);
