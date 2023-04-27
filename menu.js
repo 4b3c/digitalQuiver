eb4897e93c8c43f9b83f4dd982d44adc326926cb
Abram <58718191+4b3c@users.noreply.github.com>
2023-04-26 19:07:18 -0700

    made the pretty cool menu transition

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
-const rectangle = document.querySelector('.menu');
 let rotation = 0;
 
-window.addEventListener('wheel', function(event) {
-  const delta = event.deltaY / 10;
-  rotation += delta;
-  rectangle.style.transform = `rotate(${rotation}deg)`;
-});
+window.addEventListener("wheel", () => {
+  rotation += event.deltaY / 1100;
+  console.log(rotation);
+  document.body.style.setProperty("--scroll", rotation);
+
+}, false);
