7cda9e3ac18cdc3cff64379930752b0096a7ea93
Abram <58718191+4b3c@users.noreply.github.com>
2023-04-27 21:44:30 -0700

    added breif project example

--- menu.js
+++ menu.js
@@ -1,20 +1,5 @@
-// let rotation = 0;
-
-// window.addEventListener("wheel", () => {
-//   rotation += event.deltaY / 1150;
-//   if (rotation > 1) {rotation = 1}
-//   if (rotation < 0) {rotation = 0}
-//   document.body.style.setProperty("--scroll", rotation);
-
-// }, false);
-
-window.addEventListener(
-"scroll",
-() => {
-document.body.style.setProperty(
-"--scroll",
-window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-);
-},
-false
-);
+window.addEventListener("scroll", () => {
+  document.body.style.setProperty("--scroll",
+    window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+  );
+}, false);
