b68c82b71bbf80870bf144a3b1d66f59f754c803
Abram <58718191+4b3c@users.noreply.github.com>
2023-04-27 20:48:09 -0700

    added the second page and made menu thing work

--- menu.js
+++ menu.js
@@ -1,9 +1,20 @@
-let rotation = 0;
+// let rotation = 0;
 
-window.addEventListener("wheel", () => {
-  rotation += event.deltaY / 1150;
-  if (rotation > 1) {rotation = 1}
-  if (rotation < 0) {rotation = 0}
-  document.body.style.setProperty("--scroll", rotation);
+// window.addEventListener("wheel", () => {
+//   rotation += event.deltaY / 1150;
+//   if (rotation > 1) {rotation = 1}
+//   if (rotation < 0) {rotation = 0}
+//   document.body.style.setProperty("--scroll", rotation);
 
-}, false);
+// }, false);
+
+window.addEventListener(
+"scroll",
+() => {
+document.body.style.setProperty(
+"--scroll",
+window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+);
+},
+false
+);
