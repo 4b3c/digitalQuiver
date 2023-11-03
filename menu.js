f32fe9cd6963afb088d56092e01db125415890f8
Abram <58718191+4b3c@users.noreply.github.com>
2023-11-03 15:31:18 -0700

    scroll animation no longer relative to page length

--- menu.js
+++ menu.js
@@ -1,6 +1,17 @@
+const menu = document.querySelector('.menu');
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.pageYOffset;
+  const animationStartPosition = 0; // Set the pixel value where the animation should start
+  const animationEndPosition = 750; // Set the pixel value where the animation should end
 
-window.addEventListener("scroll", () => {
-  document.body.style.setProperty("--scroll",
-    window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-  );
-}, false);
+  // Calculate the normalized value between 0 and 1 based on the scroll position
+  let normalizedScroll = (scrollPosition - animationStartPosition) / (animationEndPosition - animationStartPosition);
+  
+  // Ensure the normalized value stays within the [0, 1] range
+  normalizedScroll = Math.min(1, Math.max(0, normalizedScroll));
+
+  document.body.style.setProperty('--scroll', normalizedScroll);
+  
+  // Adjust the animation delay based on the scroll position
+  menu.style.animationDelay = `-${normalizedScroll * menu.style.animationDuration}`;
+});
