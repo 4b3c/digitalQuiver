dc9037e3b05cc32f618ed06ba73b86fd42eddb0f
Abram <58718191+4b3c@users.noreply.github.com>
2025-03-28 16:28:05 -0700

    hope this fixes it

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const menu = document.querySelector('.menu');
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.pageYOffset;
+  const animationStartPosition = 0;
+  const animationEndPosition = 750;
+
+  let normalizedScroll = (scrollPosition - animationStartPosition) / (animationEndPosition - animationStartPosition);
+  normalizedScroll = Math.min(1, Math.max(0, normalizedScroll));
+  document.body.style.setProperty('--scroll', normalizedScroll);
+
+  if (menu && menu.style.animationDuration) {
+    menu.style.animationDelay = `-${normalizedScroll * parseFloat(menu.style.animationDuration)}s`;
+  }
+});
 
 fetch('data/projects.json')
   .then(response => response.json())
